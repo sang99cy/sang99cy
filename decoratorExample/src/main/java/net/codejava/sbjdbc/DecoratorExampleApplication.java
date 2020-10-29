a300d9b192498ce604249bfbd6b4c0763201c254
khuatquansang <quangsang99hn@gmail.com>
2020-10-29 17:34:32 +0700

    commit code về thymeleaf + spring boot lần đầu tiên

---
+++ decoratorExample/src/main/java/net/codejava/sbjdbc/DecoratorExampleApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.sbjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoratorExampleApplication.class, args);
+	}
+
+}
